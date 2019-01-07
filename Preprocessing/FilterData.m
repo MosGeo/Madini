dbaa380f78e49d0a6d6bb02bf0cd490bf2a18c3c
Mustafa Al Ibrahim <MosGeo@users.noreply.github.com>
2019-01-07 10:16:44 -0800

    Organized function

--- Preprocessing/FilterData.m
+++ Preprocessing/FilterData.m
@@ -1,20 +1,36 @@
-function dataTable = FilterData(dataTable,elementsToUse)
-%FilterData Filter data to specific elements.
-%   data = FilterData(data, header,elementsToUse) returns filtered data
-%   with specific elements and renormalizes the remaining Data.
+function dataTable = FilterData(dataTable, columnsToUse, isNormalize)
+%% FILTERDATA   filter data with specific columns with renormalizes 
 %
-%   Mustafa Al Ibrahim (Mustafa.Geoscientist@outlook.com)
-%   Febuary, 2017
+% dataTable:                Table containing data
+% columnsToUse:             Column names to keep
+% isRenormalize:            Normalize the remaining columns to 1?
+%
+% Mustafa Al Ibrahim @ 2018
+% Mustafa.Geoscientist@outlook.com
+
+%% Preprocessing
+
+% Defaults
+if ~exist('isNormalize', 'var'); isNormalize = true; end
+
+% Assertions
+assert(exist('dataTable', 'var')== true, 'dataTable must be provided');
+assert(istable(dataTable), 'dataTable must be a table');
+assert(exist('columnsToUse', 'var')== true, 'columnsToUse must be provided');
+assert(iscell(columnsToUse) || ischar(columnsToUse), 'columnsToUse must a string or a cells of strings');
+assert(isa(isNormalize, 'logical'), 'isNormalize must be a logical');
 
 %% Main
 
 % Filter data to specific elements
-dataTable = dataTable(:,elementsToUse);
+dataTable = dataTable(:,columnsToUse);
 
-% Renormalize the remaining data
-dataFilteredNormalized = table2array(dataTable) ./ repmat(sum(table2array(dataTable), 2), 1, size(dataTable,2));
-
-% Insert the normalized data back to the table
-dataTable(:,:) = array2table(dataFilteredNormalized);
+if isNormalize == true
+    % Renormalize the remaining data
+    dataFilteredNormalized = table2array(dataTable) ./ repmat(sum(table2array(dataTable), 2), 1, size(dataTable,2));
+    
+    % Insert the normalized data back to the table
+    dataTable(:,:) = array2table(dataFilteredNormalized);
+end
 
 end
